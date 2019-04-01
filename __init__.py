2df87b092c6cdcb4a8eeeea8bcc0fb3c8ab46767
Hodonou SOUNTON <sounton@gmail.com>
2019-04-01 14:22:10 +0100

    It begins

---
+++ __init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Created On: 2019-04-01 12:27:15
+
+@Author: Hodonou SOUNTON (drxos), MSc IR
+
+Description: le module __init__.py est une norme en python. Il permet de faire d'un dossier, un paquet python.
+"""
+
+
+code
